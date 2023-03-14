a4e79b2202ca709f5ae995ecfe160afe5762fbc6
PedroC55 <plbc@ua.pt>
2023-03-14 17:50:17 +0000

    Critico

--- CampusApp/app/src/main/java/com/example/myapplication/LoginFragment.kt
+++ CampusApp/app/src/main/java/com/example/myapplication/LoginFragment.kt
@@ -1,4 +1,21 @@
 package com.example.myapplication
 
-class LoginFragment {
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class LoginFragment : Fragment(R.layout.login) {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.login, container, false)
+    }
+
 }
