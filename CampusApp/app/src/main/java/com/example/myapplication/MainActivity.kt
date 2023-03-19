7d8efa6754ee1e6c23793e39b70125d931d7f61d
PedroC55 <plbc@ua.pt>
2023-03-19 23:18:20 +0000

    BottomNavigation Done mas com erros

--- CampusApp/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ CampusApp/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,11 +2,41 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 
+
+
+import com.google.android.material.bottomnavigation.BottomNavigationView
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val DashboardFragment = DashboardFragment()
+        val MapFragment = MapFragment()
+        val NearByEventsFragment = NearByEventsFragment()
+        val ClassroomScheduleFragment = ClassroomScheduleFragment()
+
+        val bottomNavigationView =  findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.ic_home -> makeCurrentFragment(DashboardFragment)
+                R.id.ic_classroom -> makeCurrentFragment(ClassroomScheduleFragment)
+                R.id.ic_location -> makeCurrentFragment(MapFragment)
+                R.id.ic_social-> makeCurrentFragment(NearByEventsFragment)
+            }
+            true
+        }
+
+
+
     }
+
+    private fun makeCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.ll_wrapper, fragment)
+            commit()
+        }
 }
