3d9e0c419d76236c6c4ff5e573a1aab5984a1b77
Bernardo Leandro <bernardoleandro1@ua.pt>
2023-03-21 01:18:53 +0000

    small update

--- CampusApp/app/src/main/java/com/example/myapplication/Photos.kt
+++ CampusApp/app/src/main/java/com/example/myapplication/Photos.kt
@@ -4,8 +4,8 @@ import android.provider.ContactsContract.Contacts.Photo
 
 class Photos {
     var id : Int = 0
-    var studentOrEvent : Int = 0
-    lateinit var photo : Photo
+    private var studentOrEvent : Int = 0
+    private var photo : Photo
 
     constructor(studentOrEvent : Int, photo : Photo){
         this.studentOrEvent = studentOrEvent
